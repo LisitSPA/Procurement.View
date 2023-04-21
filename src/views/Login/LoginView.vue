6df7cccd2a7e41640ae09d2432c82eb046d5f81b
John Herrera <jherrera@lisit.cl>
2023-04-21 17:30:09 -0400

    Login

--- src/views/Login/LoginView.vue
+++ src/views/Login/LoginView.vue
@@ -1,9 +1,16 @@
 <template>
-<div class="row">
-	<div class="col-12">
-		<h1>Login view</h1>
+	<div class="row bg-login">
+		<div class="col-12 circle">
+			<div class="row login-section">
+				<div class="col-12" style="text-align-last: center;">
+					<img src="./../../assets/images/logo-azul.png" alt="Logo Procurement" style="width: 55%">
+				</div>
+				<div class="col-12 text-center" style="margin-top: 10rem;">
+					<p style="font-size: 17px">Para acceder, debes iniciar sesión</p>
+				</div>
+			</div>
+		</div>
 	</div>
-</div>
 </template>
 
 <script>
@@ -12,6 +19,23 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+.bg-login{
+	background-color: var(--dark-blue);
+	height: 100vh;
+	background-size: cover;
+}
+.circle{
+	background-color: white;
+	clip-path: circle(30% at 50% 50%);
+}
+.login-section{
+	height: 86vh;
+    display: flex;
+    margin: 7vh 44vh 7vh 44vh;
+    justify-content: center;
+    align-content: center;
+}
 
 </style>
