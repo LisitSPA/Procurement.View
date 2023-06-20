b9d3d2ae0a06ef924d3534aea1678f71c0758298
John Herrera <jherrera@lisit.cl>
2023-06-20 10:30:45 -0400

    Redirección - control de gestion

--- src/views/Secciones/Control de Gestion/Home.vue
+++ src/views/Secciones/Control de Gestion/Home.vue
@@ -7,18 +7,18 @@
 			<button class="btn btn-section-menu" @click="this.$router.push('Administracion-Contratos')">Panel de administración<br>de contratos Marco SAP</button>
 		</div>
 		<div class="col d-flex justify-content-center align-items-center">
-			<button class="btn btn-section-menu" @click="this.$router.push('Contratos-legales')">Panel de<br>contratos legales</button>
+			<button class="btn btn-section-menu" @click="this.$router.push('/Not-Found')">Panel de<br>contratos legales</button>
 		</div>
 	</div>
 	<div class="row" style="height: 25%;">
 		<div class="col d-flex justify-content-center align-items-center">
-			<button class="btn btn-section-menu" @click="this.$router.push('Coste-Drivers')">Coste drivers e<br>Indicadores de mercado</button>
+			<button class="btn btn-section-menu" @click="this.$router.push('/Not-Found')">Coste drivers e<br>Indicadores de mercado</button>
 		</div>
 		<div class="col d-flex justify-content-center align-items-center">
-			<button class="btn btn-section-menu" @click="this.$router.push('Pago-Proveedores')">Panel de<br>pago a proveedores</button>
+			<button class="btn btn-section-menu" @click="this.$router.push('/Not-Found')">Panel de<br>pago a proveedores</button>
 		</div>
 		<div class="col d-flex justify-content-center align-items-center">
-			<button class="btn btn-section-menu" @click="this.$router.push('Presupuestos')">Presupuestos</button>
+			<button class="btn btn-section-menu" @click="this.$router.push('/Not-Found')">Presupuestos</button>
 		</div>
 	</div>
 </template>
