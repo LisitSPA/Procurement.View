d00bbd9c529dfb80d15ebe6c988d9d403ec9c0e2
John Herrera <jherrera@lisit.cl>
2023-06-22 12:04:15 -0400

    Update interaccion - Preview video

--- src/components/Shared/PreviewVideoCard.vue
+++ src/components/Shared/PreviewVideoCard.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="col h-100 bg-gris2 cursor-pointer" style="margin-right: 1rem; border-radius: 40px;">
+	<div class="col h-100 bg-gris2 cursor-pointer" style="margin-right: 1rem; border-radius: 40px;" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
 		<div class="row contenedor-imagen bg-blue1" style="border-radius: 40px;">
 			<div class="col-12 d-flex justify-content-center align-items-center">
 				<i class="bi bi-play-circle text-white" style="font-size: 6rem;"></i>
@@ -8,6 +8,15 @@
 		<div class="row wea h-25">
 			<div class="col-12 d-flex justify-content-center align-items-center">
 				<span class="text-white">Dashboard Impact Tracker</span>
+			</div>
+		</div>
+	</div>
+	<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+		<div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+			<div class="modal-content">
+				<div class="modal-body" style="display: contents;">
+					<img src="https://picsum.photos/800/500" alt="Imagen">
+				</div>
 			</div>
 		</div>
 	</div>
