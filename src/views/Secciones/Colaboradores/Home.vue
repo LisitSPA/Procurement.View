19209315df12c1489a18e186e7e5fba8a20c7062
John Herrera <jherrera@lisit.cl>
2023-06-27 09:26:51 -0400

    Update interaccion - colaboradores

--- src/views/Secciones/Colaboradores/Home.vue
+++ src/views/Secciones/Colaboradores/Home.vue
@@ -21,9 +21,12 @@
 				</div>
 				<div class="col-12" style="padding: 0 10px 0 20px; height: 20vh;">
 					<div class="row h-100">
-						<div class="col user-bday bg-gris" v-for="(items,index) in 4" :key="index" >
+						<div v-if="currentPage != 1" class="col-1 d-flex align-items-center justify-content-center cursor-pointer" @click="currentPage--">
+							<i class="bi bi-caret-left-fill text-blue2" style="font-size: 22px;"></i>
 						</div>
-						<div class="col-1 d-flex align-items-center justify-content-center">
+						<div class="col user-bday bg-gris" v-for="(items,index) in displayPeople" :key="index" >
+						</div>
+						<div v-if="currentPage < totalPage" class="col-1 d-flex align-items-center justify-content-center cursor-pointer" @click="currentPage++">
 							<i class="bi bi-caret-right-fill text-blue2" style="font-size: 22px;"></i>
 						</div>
 					</div>
@@ -43,18 +46,25 @@
 				</div>
 				<div class="col-12" style="padding: 0 10px 0 20px; height: 12vh;;">
 					<div class="row h-100">
+						<div class="col-1 d-flex align-items-center justify-content-center cursor-pointer"
+							v-if="currentPage2 != 1"
+							@click="currentPage2--">
+							<i class="bi bi-caret-left-fill text-blue2" style="font-size: 22px;"></i>
+						</div>
 						<div class="col">
 							<div class="row h-100">
-								<div class="col new-user bg-gris" v-for="(items,index) in 5" :key="index">
+								<div class="col new-user bg-gris" v-for="(items,index) in displayPeople2" :key="index">
 								</div>
 							</div>
 							<div class="row">
-								<div class="col text-blue2 text-center bg-gris" v-for="(items,index) in 5" :key="index" style="margin-right: 10px; height: 1.5rem;">
+								<div class="col text-blue2 text-center bg-gris" v-for="(items,index) in displayPeople2" :key="index" style="margin-right: 10px; height: 1.5rem;">
 									<span class="fw-bold">12 abril 23</span>
 								</div>
 							</div>
 						</div>
-						<div class="col-1 d-flex align-items-center justify-content-center">
+						<div class="col-1 d-flex align-items-center justify-content-center cursor-pointer" 
+							v-if="currentPage2 < totalPage2"
+							@click="currentPage2++">
 							<i class="bi bi-caret-right-fill text-blue2" style="font-size: 22px;"></i>
 						</div>
 					</div>
@@ -65,6 +75,31 @@
 </template>
 
 <script setup>
+	import { computed, ref } from 'vue'
+	const people = ref(['person','person2','person3','person4','person5','person6','person7','person8','person9','person10','person11','person12'])
+	const people2 = ref(['person','person2','person3','person4','person5','person6','person7','person8','person9','person10','person11','person12'])
+
+	const peoplePage = ref(4)
+	const currentPage = ref(1)
+	const totalPage = people.value.length / peoplePage.value
+
+	const peoplePage2 = ref(4)
+	const currentPage2 = ref(1)
+	const totalPage2 = people2.value.length / peoplePage2.value
+
+
+	const displayPeople = computed(() => {
+		const startIndex = (currentPage.value - 1) * peoplePage.value;
+		const endIndex = startIndex + peoplePage.value; 
+		return people.value.slice(startIndex, endIndex);
+	})
+
+	const displayPeople2 = computed(() => {
+		const startIndex = (currentPage2.value - 1) * peoplePage2.value;
+		const endIndex = startIndex + peoplePage2.value; 
+		return people2.value.slice(startIndex, endIndex);
+	})
+
 
 </script>
 
