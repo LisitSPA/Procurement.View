5aef662cba721ceeebf575010f88c62a292472a1
John Herrera <jherrera@lisit.cl>
2023-07-07 16:18:56 -0400

    Se agrega nueva sección - on boarding

--- src/views/Home/Categories.vue
+++ src/views/Home/Categories.vue
@@ -4,17 +4,18 @@
 	</div>
 	<div class="row" style="height: 30vh">
 		<div class="col-12 d-flex justify-content-center align-items-center">
-			<button class="btn btn-border-blue" @click="this.$router.push('/Colaboradores')">Colaboradores</button>
-			<button class="btn btn-border-blue" @click="this.$router.push('/Control-Gestion')">Control de gestión</button>
-			<button class="btn btn-border-blue" @click="this.$router.push('/Procurement-Excellence')">Procurement Excellence</button>
-			<button class="btn btn-border-blue" @click="this.$router.push('/Proyectos-de-Valor')">Proyectos de Valor</button>
+			<button class="btn btn-border-blue" @click="navigate('/Colaboradores')">Colaboradores</button>
+			<button class="btn btn-border-blue" @click="navigate('/Control-Gestion')">Control de gestión</button>
+			<button class="btn btn-border-blue" @click="navigate('/Procurement-Excellence')">Procurement Excellence</button>
+			<button class="btn btn-border-blue" @click="navigate('/Proyectos-de-Valor')">Proyectos de Valor</button>
+			<button class="btn btn-border-blue" @click="navigate('/Not-Found')">On Boarding</button>
 		</div>
 		<div class="col-12 d-flex justify-content-center align-items-center">
-			<button class="btn btn-border-blue" @click="this.$router.push('/Not-Found')">Sostenibilidad</button>
-			<button class="btn btn-border-blue" @click="this.$router.push('/Not-Found')">Analytics, Tools and Data</button>
-			<button class="btn btn-border-blue" @click="this.$router.push('/Not-Found')">Procurement Academy</button>
-			<button class="btn btn-border-blue" @click="this.$router.push('/Not-Found')">Proveedores</button>
-			<button class="btn btn-border-blue" @click="this.$router.push('/Not-Found')">Procurement 4.0</button>
+			<button class="btn btn-border-blue" @click="navigate('/Not-Found')">Sostenibilidad</button>
+			<button class="btn btn-border-blue" @click="navigate('/Not-Found')">Analytics, Tools and Data</button>
+			<button class="btn btn-border-blue" @click="navigate('/Not-Found')">Procurement Academy</button>
+			<button class="btn btn-border-blue" @click="navigate('/Not-Found')">Proveedores</button>
+			<button class="btn btn-border-blue" @click="navigate('/Not-Found')">Procurement 4.0</button>
 		</div>
 	</div>
 	<div class="col-12 d-flex align-items-end justify-content-center">
@@ -23,6 +24,13 @@
 </template>
 
 <script setup>
+	import {useRouter} from 'vue-router'
+	
+	const router = useRouter()
+
+	const navigate = (option) => {
+		router.push(option)
+	}
 
 </script>
 
